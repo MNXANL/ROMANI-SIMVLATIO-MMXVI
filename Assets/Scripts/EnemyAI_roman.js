91eeab92a61d29ce48d1710d4cd5b86ba99a24ae
Miguel Moreno Gómez <miguelmoregome@gmail.com>
2016-07-05 13:56:08 +0200

    Updated enemy soldier behaviour 2

--- Assets/Scripts/EnemyAI_roman.js
+++ Assets/Scripts/EnemyAI_roman.js
@@ -1,6 +1,7 @@
-﻿#pragma strict
+#pragma strict
 
 private var player : GameObject;
+//public var anim : Animator;
 
 private var t : float = 0.0f;
 
@@ -10,9 +11,45 @@ function Start () {
 
 function Update () {
 	t += Time.deltaTime;
-	//if (t > .5) {
+	if (t > .5) {
 		transform.LookAt(player.transform);
 		t = 0;
-	//}
-	transform.position (1, 0, 0);
+	}
 }
+
+/*
+function Update () {
+	transform.LookAt(player.transform);
+	var hitInfo : RaycastHit;
+	if (Physics.Raycast(transform.position, transform.forward, hitInfo))	{
+		if(hitInfo.collider.gameObject.CompareTag("Player")) {
+			seeingPlayer = true;
+			dist = hitInfo.distance;
+			Debug.Log("DIST = " + rateOfFire/dist);
+		}
+		else seeingPlayer = false;
+	}
+
+	//Si ve al jugador, el ca��n se pone en verde. En caso contrario, se pone en azul.
+
+	var mrs:MeshRenderer[] = GetComponentsInChildren.<MeshRenderer>();
+	for (var i:int = 0; i < mrs.length; ++i) {
+		 if(seeingPlayer) mrs[i].material.color = Color.green;
+		 else mrs[i].material.color = Color.blue;
+	}
+
+
+	//Dispara una mina, de la misma forma que en el FPS del dia 1.
+	
+	time += Time.deltaTime;
+	if(time > (rateOfFire - rateOfFire/dist) && seeingPlayer)	{
+		
+		time = 0.0f;
+		var instantiatedBullet : GameObject = GameObject.Instantiate(bulletPrefab, transform.position, transform.rotation);
+		for (i = 0; i < mrs.length; ++i)  mrs[i].material.color = Color.green;
+		var rb : Rigidbody = instantiatedBullet.GetComponent.<Rigidbody>();
+		rb.velocity = transform.forward * bulletSpeed;
+	}
+}
+
+*/
