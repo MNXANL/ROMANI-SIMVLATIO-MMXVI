1d0b9973731fefb1a1dc9a55700ddc780581a7df
Miguel Moreno Gómez <miguelmoregome@gmail.com>
2016-07-05 12:44:51 +0200

    First files uploaded

---
+++ Assets/Scripts/Reset.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	if (Input.GetKeyDown(KeyCode.R)) {
+		SceneManager.LoadScene("Game1");
+	}
+	else if (Input.GetKeyDown(KeyCode.Escape)) {
+		SceneManager.LoadScene("Menu");
+	}
+}
